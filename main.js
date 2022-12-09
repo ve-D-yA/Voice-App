33153dd3455f37e826376012929fb4b673829a41
ve-D-yA <108597415+ve-D-yA@users.noreply.github.com>
2022-12-08 20:14:13 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -17,19 +17,30 @@ recognition.onresult = function(event)
 
     document.getElementById("textbox").innerHTML = Content;
     console.log(Content);
-    speak();
+        if(Content == "take my selfie")
+        {
+            console.log("taking selfie");
+            speak();
+        }
 }
 
 function speak()
 {
     var synth = window.speechSynthesis;
 
-    speak_data = document.getElementById("textbox").value;
+    speak_data = "Taking you Selfie in 5 seconds";
 
     var utterThis = new SpeechSynthesisUtterance(speak_data);
 
     synth.speak(utterThis);
-    WebKitCSSMatrix.attach(camera);
+    Webcam.attach(camera);
+
+    setTimeout(function()
+    {
+        take_snapshot();
+        save();
+    }, 5000);
+
 }
 
 Webcam.set({
@@ -39,3 +50,19 @@ Webcam.set({
     png_quality:90
 });
 camera = document.getElementById("camera");
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_url)
+    {
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_url+'">'
+    });
+}
+
+function save()
+{
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+}
